1c435ebdfed97ced659683df6f6699fadbaffb3f
Costantino14 <costantino.grabesu14@gmail.com>
2024-07-23 14:41:50 +0200

    modifica profile

--- Frontend-blog/src/views/Profile.jsx
+++ Frontend-blog/src/views/Profile.jsx
@@ -1,42 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import "./style.css";
-import { deleteAuthor, deletePost, getPosts } from '../services/api';
+import { deleteAuthor, deletePost, getPosts, getUserData } from '../services/api';
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import { HiOutlineTrash } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom';
 import ModalPost from '../components/ModalPost';
 
-export default function Profile(listAuthors) {
+export default function Profile() {
   const [posts, setPosts] = useState([]);
-  const [foundAuthor, setFoundAuthor] = useState(null);
+  const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
 
-
-  const data = localStorage.getItem("data");
-  
   useEffect(() => {
-    if (data && listAuthors.listAuthors) {
-      const list = listAuthors.listAuthors;
-      const author = list.find(author => author.email.toLowerCase() === data.toLowerCase());
-      setFoundAuthor(author || null);
-    }
-  }, [data, listAuthors]);
-
-  useEffect(() => {
-    const FetchBlogPost = async () => {
+    const fetchUserDataAndPosts = async () => {
       try {
-        const data = await getPosts();
-        setPosts(data.blogPosts);
+        const userDataResponse = await getUserData();
+        setUserData(userDataResponse);
+
+        const postsData = await getPosts();
+        setPosts(postsData.blogPosts);
       } catch (error) {
-        console.error("Errore nel recupero dei dati utente:", error);
-      }    
+        console.error("Errore nel recupero dei dati:", error);
+        navigate("/login");
+      }
     };
-  
-    FetchBlogPost(); 
-  }, []);
+
+    fetchUserDataAndPosts();
+  }, [navigate]);
 
   const handleDeleteAuthor = async (id) => {
-    if (!foundAuthor) {
+    if (!userData) {
       console.error("Autore non trovato");
       return;
     }
@@ -48,13 +41,12 @@ export default function Profile(listAuthors) {
         alert("Hai cancellato il tuo profilo!")
         navigate("/");
       } else {
-        console.error("Errore nella cancellazione del autore, passaggio errato");
+        console.error("Errore nella cancellazione dell'autore");
       }
     } catch (error) {
-      console.error("Errore nella cancellazione del autore:", error.response?.data || error.message);
+      console.error("Errore nella cancellazione dell'autore:", error);
     }
   };
-
 
   const handleDeletePost = async (id) => {
     if (!id) {
@@ -67,70 +59,60 @@ export default function Profile(listAuthors) {
         setPosts(prev => prev.filter(post => post._id !== id));
         alert("Post cancellato con successo!")
       } else {
-        console.error("Errore nella cancellazione del post, passaggio errato");
+        console.error("Errore nella cancellazione del post");
       }
     } catch (error) {
-      console.error("Errore nella cancellazione del post:", error.response?.data || error.message);
+      console.error("Errore nella cancellazione del post:", error);
     }
   };
 
-
-  console.log(posts);   
-  console.log(foundAuthor);
-
-  if (!foundAuthor) {
+  if (!userData) {
     return <div>Caricamento...</div>;
   }
 
   return (
     <div className='root'>
       <Container>
-      <div className='profile-center'>
-      <h1 className='title-profile'>Profilo</h1>
-      
-      <Card className="profile-card">
-      <div className="profile-image-container">
-        <Card.Img variant="top" src={foundAuthor.avatar} className="profile-image" />
-      </div>
-      <Card.Body>
-        <Card.Title>{foundAuthor.nome} {foundAuthor.cognome}</Card.Title>
-        <Card.Text>
-            Data di Nascita: {foundAuthor.dataNascita} <br />
-            E-mail: {foundAuthor.email}
-        </Card.Text>
-          <Button className="ms-1 buttons-profile" variant="outline-success" ><HiOutlineTrash /></Button>
-          <Button className="ms-1 buttons-profile" variant="outline-danger" onClick={() =>handleDeleteAuthor(foundAuthor._id)}><HiOutlineTrash /></Button>
-      </Card.Body>
-    </Card>
-    </div>
-
-    {posts.filter(post => post.authorEmail.toLowerCase().includes(foundAuthor.email))
-  .map((post) => (
-    
-    <Card className="post-profile-card horizontal-card">
-    <Row noGutters>
-      <Col md={4}>
-        <div className="post-profile-image-container">
-          <Card.Img src={post.cover} className="post-profile-cover" />
+        <div className='profile-center'>
+          <h1 className='title-profile'>Profilo</h1>
+          
+          <Card className="profile-card">
+            <div className="profile-image-container">
+              <Card.Img variant="top" src={userData.avatar} className="profile-image" />
+            </div>
+            <Card.Body>
+              <Card.Title>{userData.nome} {userData.cognome}</Card.Title>
+              <Card.Text>
+                Data di Nascita: {userData.dataNascita} <br />
+                E-mail: {userData.email}
+              </Card.Text>
+              <Button className="ms-1 buttons-profile" variant="outline-danger" onClick={() => handleDeleteAuthor(userData._id)}><HiOutlineTrash /></Button>
+            </Card.Body>
+          </Card>
         </div>
-      </Col>
-      <Col md={8}>
-        <Card.Body className='post-profile-body'>
-          <Card.Title className='post-profile-title'>{post.title}</Card.Title>
-          <Card.Text className='post-profile-text'>{post.category}</Card.Text>
-          <Card.Text className='post-profile-text'>{post.content.substring(0, 100)}...</Card.Text>
-          <ModalPost id={post._id} />          
-          <Button className="ms-1 buttons-profile" variant="outline-danger" onClick={() =>handleDeletePost(post._id)}><HiOutlineTrash /></Button>
-        </Card.Body>
-      </Col>
-    </Row>
-  </Card>
-  ))}
-  </Container>
+
+        {posts.filter(post => post.authorEmail.toLowerCase() === userData.email.toLowerCase())
+          .map((post) => (
+            <Card key={post._id} className="post-profile-card horizontal-card">
+              <Row noGutters>
+                <Col md={4}>
+                  <div className="post-profile-image-container">
+                    <Card.Img src={post.cover} className="post-profile-cover" />
+                  </div>
+                </Col>
+                <Col md={8}>
+                  <Card.Body className='post-profile-body'>
+                    <Card.Title className='post-profile-title'>{post.title}</Card.Title>
+                    <Card.Text className='post-profile-text'>{post.category}</Card.Text>
+                    <Card.Text className='post-profile-text'>{post.content.substring(0, 100)}...</Card.Text>
+                    <ModalPost id={post._id} />          
+                    <Button className="ms-1 buttons-profile" variant="outline-danger" onClick={() => handleDeletePost(post._id)}><HiOutlineTrash /></Button>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+          ))}
+      </Container>
     </div>
   )
 }
-
-
-
-   
